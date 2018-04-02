6cb814bdeade1dd3a51b4377c0757f64b18a1f9b
kyungmi.k <kyungmi.k@ridi.com>
2018-04-02 16:45:19 +0900

    `ScopeChecker` 클래스 메서드 수정
    - `and` => `every`: `and`는 PHP 예약어이므로 `every`로 변경
    - `some`: 코멘트를 명확한 메서드 명으로 표현

--- lib/Authorization/Validator/ScopeChecker.php
+++ lib/Authorization/Validator/ScopeChecker.php
@@ -6,37 +6,46 @@ use Ridibooks\OAuth2Resource\Constant\ScopeConstant;
 class ScopeChecker
 {
     /**
-     * @param array $require_scopes
-     * @param array $user_scope
+     * @param array $required_scopes
+     *    e.g. ['a', 'b', ['c', 'd']] => 'a' or 'b' or ('c' and 'd')
+     * @param array $user_scopes
      * @return bool
      */
-    public static function check(array $require_scopes, array $user_scope): bool
+    public static function check(array $required_scopes, array $user_scopes): bool
     {
-        if (in_array(ScopeConstant::SCOPE_FULL_AUTHORITY, $user_scope)) {
+        if (in_array(ScopeConstant::SCOPE_FULL_AUTHORITY, $user_scopes)) {
             return true;
         }
 
-        // Or 확인
-        foreach ($require_scopes as $require_scope) {
-            if (is_array($require_scope) && self::and($require_scope, $user_scope)) {
+        return self::some($required_scopes, $user_scopes);
+    }
+
+    /**
+     * @param array $required_scopes
+     * @param array $user_scopes
+     * @return bool
+     */
+    private static function some(array $required_scopes, array $user_scopes): bool
+    {
+        foreach ($required_scopes as $required_scope) {
+            if (is_array($required_scope) && self::every($required_scope, $user_scopes)) {
                 return true;
-            } elseif (in_array($require_scope, $user_scope)) {
+            } elseif (in_array($required_scope, $user_scopes)) {
                 return true;
             }
         }
-
         return false;
     }
 
     /**
-     * @param array $require_scopes
-     * @param array $user_scope
+     * @param array $required_scopes
+     * @param array $user_scopes
      * @return bool
      */
-    private static function and (array $require_scopes, array $user_scope): bool
+    private static function every(array $required_scopes, array $user_scopes): bool
     {
-        foreach ($require_scopes as $require_scope) {
-            if (!in_array($require_scope, $user_scope)) {
+        foreach ($required_scopes as $required_scope) {
+            if (!in_array($required_scope, $user_scopes)) {
                 return false;
             }
         }
